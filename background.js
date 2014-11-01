d044aeb2614656ea6fe55b44777897a4363fafd4
naiduv <naiduvenkat@gmail.com>
2014-11-01 16:42:56 -0400

    changes for the chrome app version

--- background.js
+++ background.js
@@ -1,24 +1,8 @@
-chrome.extension.onMessage.addListener(msgHandler);
-
-var starttime = new Date().getTime();
-var new_top_score = 0;
-
-var skierloc = new Point(0,0);
-var map = [];
-
-var msgHandler = function(e){
-	if(!e || !e.msg)
-		return;
-	switch(e.msg){
-		case 'new_top_score':
-			new_top_score = e.value;
-			lasttime = new Date().getTime();
-			break;
-		case 'current_state':
-			val = e.value;
-			break;
-		case 'get_old_state'
-			//send_old_state();
-			break;
-	}
-}
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('popup.html', {
+    'bounds': {
+      'width': 400,
+      'height': 300
+    }
+  });
+});
